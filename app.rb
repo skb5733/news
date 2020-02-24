b5fbdb1e239ae9203954a8dbde21ceba41d3b12e
skb5733 <spencer.brown1@kellogg.northwestern.edu>
2020-02-24 20:56:15 +0000

    3pmfeb24

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ before { puts "Parameters: #{params}" }
 
 # enter your Dark Sky API key here
 ForecastIO.api_key = "cdd0def2e32890be9e721b76f2fb46e5"
+forecast = ForecastIO.forecast(46.5,-87.5).to_hash
 
 get "/" do
   # show a view that asks for the location
@@ -19,9 +20,24 @@ get "/news" do
     results = Geocoder.search(params["location"])
     @lat_long = results.first.coordinates # => [lat, long]
 
-    # lat = lat_long[0]
-    # long = lat_long[1]
-    # "#{lat_long[0]} #{lat_long[1]}"  
+    lat = @lat_long[0]
+    long = @lat_long[1]
+
+    # forecast = ForecastIO.forecast(lat,long).to_hash
+    
+    # current_summary = forecast["currently"]["summary"]
+    # current_temp = forecast["currently"]["temperature"]
+    
+    # puts "Today it is #{forecast["currently"]["temperature"]} and #{forecast["currently"]["temperature"]}"     
+
+    # forecast_array = forecast["daily"]["data"]
+    
+    # for weather_for_the_day in forecast_array
+    # puts "A high temperature of #{weather_for_the_day["temperatureHigh"]} and #{weather_for_the_day["summary"]}."
+    # end
+
+    # "#{@lat_long[0]} #{@lat_long[1]}"  
     # puts @lat_long
  
+    view "news"
 end
